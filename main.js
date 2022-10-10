90eaf13d5c0c5e6687fb2d19870b280490a41941
Richard Araujo <111157511+richardaraujob@users.noreply.github.com>
2022-10-10 10:12:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
+const operation = document.querySelector("#operation");
+const result = document.querySelector("#result");
 
-function clickNumber(numberButtons) {
-    const number = document.getElementById("operation").innerHTML;
-    const value = number + numberButtons;
-    document.getElementById("operation").innerHTML = value;
+function clickButton(btn) {
+    operation.innerHTML += btn;
 };
 
 function clearOperation() {
-    document.getElementById("operation").innerHTML = "";
-    document.getElementById("result").innerHTML = "";
+    operation.innerHTML = '';
+    result.innerHTML = '';
 };
 
-function calculo() {
-    const resultado = document.getElementById("operation").innerHTML;
-    if(resultado) {
-        document.getElementById("result").innerHTML = eval(resultado);
+function calc() {
+    if(operation.textContent) {
+        result.innerHTML = eval(operation.innerHTML);
     } else {
-        document.getElementById("result").innerHTML = "Digite...";
+        result.innerHTML = "Digite...";
     };
 };
 
+function backSpace() {
+    const backspace = operation.innerHTML;
+    operation.innerHTML = backspace.substring(0, backspace.length - 1);
+};
 
 
+const check = document.getElementById("check");
 
-
-
+check.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+    document.querySelector("#calculator").classList.toggle("light");
+    document.querySelector("#container-buttons").classList.toggle("light");
+    document.querySelector("#operation").classList.toggle("light");
+    document.querySelector("#result").classList.toggle("light");
+    document.querySelector(".label").classList.toggle("light");
+    document.querySelector(".ball").classList.toggle("light");
+    document.querySelector(".button").classList.toggle("light");
+} );
